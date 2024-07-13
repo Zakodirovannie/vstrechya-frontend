62e5cb1266a31e3b7b65ea8fa6eef8d9b7874995
xMAPKEPx <russia201554321@gmail.com>
2024-07-14 02:36:34 +0500

    attempt to cookie-auth

--- src/components/Login-components/Authorization/LoginForm.jsx
+++ src/components/Login-components/Authorization/LoginForm.jsx
@@ -5,7 +5,6 @@ import vkIcon from '../../../assets/vkIcon.png'
 import {login} from "../../../api.auth";
 import {useDispatch} from "react-redux";
 import {setAuth} from "../../../redux/AuthSlice/AuthSlice";
-import {useNavigate} from "react-router-dom";
 import {getCookie} from "../../../cookie";
 
 const LoginForm = () => {
@@ -16,7 +15,6 @@ const LoginForm = () => {
     const [errorMessage, setErrorMessage] = useState('')
     const [accessToken, setAccessToken] = useState(null);
     const dispatch = useDispatch()
-    const navigate = useNavigate()
 
     const handleSubmit = async(evt) => {
         evt.preventDefault()
@@ -26,7 +24,7 @@ const LoginForm = () => {
             setIsError(false)
             setAccessToken(getCookie('access_token'))
             dispatch(setAuth(accessToken !== null))
-            navigate('/profile')
+            window.location.assign('https://vstrecya.space/')
         } catch (e) {
             if (e.response && (e.response.status === 401 || e.response.status === 400)) {
                 setIsError(true);
