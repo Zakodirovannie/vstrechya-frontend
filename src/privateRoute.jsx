908d835870fbe25ceca596ac2d640d0ece7d5ba5
xMAPKEPx <russia201554321@gmail.com>
2024-07-14 16:50:04 +0500

    129

--- src/privateRoute.jsx
+++ src/privateRoute.jsx
@@ -3,7 +3,7 @@ import {useSelector} from "react-redux";
 
 const PrivateRoute = () => {
   const isAuth = useSelector((state) => state.auth.isAuth)
-
+  console.log('isAuth: ', isAuth)
   if (isAuth) {
     return <Outlet/>
   } else {
